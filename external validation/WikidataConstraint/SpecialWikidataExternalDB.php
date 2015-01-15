6d4dfb446da3d8752f46d665c84217640bbf8f54
Dominic Sauer <dominic.sauer@student.hpi.de>
2015-01-15 14:22:31 +0100

    refactor ui

--- external validation/WikidataConstraint/SpecialWikidataExternalDB.php
+++ external validation/WikidataConstraint/SpecialWikidataExternalDB.php
@@ -19,37 +19,40 @@ class SpecialWikidataExternalDB extends SpecialPage {
 
 		$out = $this->getContext()->getOutput();
 
-		$id = new ItemId( $par );
-		$lookup = WikibaseRepo::getDefaultInstance()->getStore()->getEntityLookup();
-		$entity = $lookup->getEntity($id);
-		$statements = $this->suggestByItem( $entity );
-
-
-		$out->addWikiText( $statements );
-
-
-
-		/*$out->addModules( 'ext.WikidataExternalDB' );
+		//$out->addModules( 'ext.WikidataExternalDB' );
 		$out->addWikiMsg( 'wikidataconstraint-summary' );
 		$out->addHTML( '<p>Just enter an entity and let it crosscheck against MusicBrainz.<br/>'
 			. 'Try for example <i>Qxx</i> (John Lennon) and <i>Qxx</i> (Imagine)'
 			. ' and look at the results.</p>'
 		);
 
-		$out->addHTML( "<input placeholder='Qxx' id='item-input' autofocus>" );
-		$out->addHTML( "<input type='button' value='Cross-check' id='check-item-btn'> </input>" );
-		$out->addHTML( "<p/>" );
+		$out->addHTML( "<form name='ItemIdForm' action='" . $_SERVER['PHP_SELF'] . "' method='post'>" );
+		$out->addHTML( "<input placeholder='Qxx' name='itemId' id='item-input'>" );
+		$out->addHTML( "<input type='submit' value='Cross-check' id='check-item-btn' />" );
+		$out->addHTML( "</form>" );
+		/*$out->addHTML( "<p/>" );
 		$out->addHTML( "<ul id='results-list'></ul>" );
 		$out->addHTML( "<p/>" );
-		$out->addHTML( "<div id='result'></div>" );
-		*/
+		$out->addHTML( "<div id='result'></div>" );*/
+
+		if (isset($_POST['itemId'])) {
+			$id = new ItemId( $_POST['itemId'] );
+			$lookup = WikibaseRepo::getDefaultInstance()->getStore()->getEntityLookup();
+			$entity = $lookup->getEntity($id);
+			$statementIds = $this->getStatementIds( $entity );
+
+			foreach ( $statementIds as $id) {
+				$out->addWikiText( $id );
+			}
+		}
 	}
 
-	function suggestByItem( Item $item) {
+	function getStatementIds( Item $item ) {
 		$snaks = $item->getStatements()->getAllSnaks();
+		$numericIds = array();
 		foreach ( $snaks as $snak ) {
-			return $snak->getType();
+			$numericIds[] = $snak->getPropertyId()->getNumericId();
 		}
-		return;
+		return $numericIds;
 	}
 }
