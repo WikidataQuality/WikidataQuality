0e665a2ff7ec79981c7c4756b870c2e260d68b2e
Dominic Sauer <dominic.sauer@student.hpi.de>
2015-01-26 17:56:13 +0100

    refactor corss checker: only renaming, etc.

--- external validation/WikidataConstraint/CrossChecker.php
+++ external validation/WikidataConstraint/CrossChecker.php
@@ -32,102 +32,113 @@ class CrossChecker {
         );
 
         /*** get lookup & lookup entity ***/
-        $wd_lookup = WikibaseRepo::getDefaultInstance()->getStore()->getEntityLookup();
+        $wd_lookup = WikibaseRepo::getDefaultInstance()->getStore()->getEntityLookup();     //TODO make global
         $wd_item_id = new ItemId( $item_id_string );
-        $wd_item = $wd_lookup->getEntity($wd_item_id); // Wir vergleichen nur Items, hier ist ess aber noch eine Entity
+        $wd_item = $wd_lookup->getEntity($wd_item_id);
 
         /*** validate item ***/
         $this->validate($wd_item, $mapping);
 
+        /*** result is an array of strings ***/
         return $result;
     }
 
-    function validate(Item $wd_item, Array $mapping) {
-        $wd_claims = $this->getClaims($wd_item);
+    function validate( Item $wd_item, Array $mapping ) {
+        /*** get claims belonging to this wd_item ***/
+        $wd_item_claims = $this->getClaims( $wd_item );
 
-
+        /*** get db connection ***/
         wfWaitForSlaves();
         $loadBalancer = wfGetLB();
         $db = $loadBalancer->getConnection( DB_MASTER );
 
-        foreach (array_keys($mapping) as $wd_property_id) {
-            if (in_array($wd_property_id, array_keys($wd_claims))) {
+        /*** get external entity for wd_item which has an item for crosscheck and crosscheck both items ***/
+        foreach ( array_keys( $mapping ) as $wd_identifier_pid ) {
+            if ( in_array( $wd_identifier_pid, array_keys( $wd_item_claims ) ) ) {
+                $external_entity = $this->getExternalEntity( $db, $wd_identifier_pid, $wd_item_claims[$wd_identifier_pid]->getValue() );
 
-                $extEntity = $this->getExternalEntity( $db, $wd_property_id, $wd_claims[$wd_property_id]->getValue() );
-
-                $this->crosscheck_items($wd_property_id, $wd_claims, $extEntity, $mapping);
+                $this->crosscheck_items( $wd_identifier_pid, $wd_item_claims, $external_entity, $mapping );
             }
         }
     }
 
     function getClaims(Item $wd_item) {
+        /*** get the claims for every statement of the item ***/
         $wd_item_claims = array();
         $wd_item_statements = $wd_item->getStatements();
 
         foreach( $wd_item_statements as $statement ) {
             $claim = $statement->getClaim();
-            $pid = $claim->getPropertyId();
-            if ($claim->getMainSnak()->getType() == "value") {
-                $value = $claim->getMainSnak()->getDataValue();
+            $claim_pid = $claim->getPropertyId();
+            $mainSnak = $claim->getMainSnak();
 
-                $wd_item_claims[$pid->getNumericId()] = $value;
+            /*** get data value only if it's a PropertyValueSnak
+             * and not a PropertySomeValueSnak or PropertyNoValueSnak ***/
+            if ($mainSnak->getType() == "value") {
+                $value = $mainSnak->getDataValue();
+                $wd_item_claims[$claim_pid->getNumericId()] = $value;
             }
         }
         return $wd_item_claims;
     }
 
-    function crosscheck_items($wd_identifier_pid, Array $wd_item_claims, $ext_item, Array $mapping) {
+    function crosscheck_items( $wd_identifier_pid, Array $wd_item_claims, $external_item, Array $mapping ) {
         /*global $mapping;*/
         global $result;
 
-        $wd_validatable_properties = array_keys($mapping[$wd_identifier_pid]); //Example: Array containing 569
+        $wd_validatable_properties = array_keys( $mapping[$wd_identifier_pid] );
         $wd_validatable_claims = array();
 
-        foreach ($wd_validatable_properties as $wd_property_id) {
-            if (in_array($wd_property_id, array_keys($wd_item_claims))) {
-                $wd_validatable_claims[$wd_property_id] = $wd_item_claims[$wd_property_id];
+        /*** get validatable claims of wd_item ***/
+        foreach ( $wd_validatable_properties as $wd_validatable_pid ) {
+            if ( in_array( $wd_validatable_pid, array_keys( $wd_item_claims ) ) ) {
+                $wd_validatable_claims[$wd_validatable_pid] = $wd_item_claims[$wd_validatable_pid];
             }
         }
 
+        /*** check items and print out result ***/
+        foreach ( array_keys($wd_validatable_claims) as $wd_validatable_pid ){
+            $reflect = new ReflectionClass( $wd_validatable_claims[$wd_validatable_pid] );
 
-        foreach (array_keys($wd_validatable_claims) as $wd_property_id){
-            $reflect = new ReflectionClass($wd_validatable_claims[$wd_property_id]);
             switch( $reflect->getShortName() ) {
+                /*** get terms (label + aliases) of related wd_item if claim contains EntityIdValue and get result ***/
                 case "EntityIdValue":
-                    $entityId = $wd_validatable_claims[$wd_property_id]->getEntityId();
-                    $terms = $this->getTerms( $entityId );
-                    $extValues = $this->evaluateMapping( $ext_item, $mapping[$wd_identifier_pid][$wd_property_id] );
-                    if ( count( array_intersect($terms, $extValues) ) > 0 ) {
+                    $wd_entity_id = $wd_validatable_claims[$wd_validatable_pid]->getEntityId();
+                    $wd_entity_terms = $this->getTerms( $wd_entity_id );
+                    $external_values = $this->evaluateMapping( $external_item, $mapping[$wd_identifier_pid][$wd_validatable_pid] );
+                    if ( count( array_intersect( $wd_entity_terms, $external_values ) ) > 0 ) {
                         print "true";
                     }
                     break;
 
+                /*** directly get the result if claim contains StringValue ***/
                 case "StringValue":
                     break;
             }
         }
     }
 
-    function getTerms( $itemId ){
-        $lookup = WikibaseRepo::getDefaultInstance()->getStore()->getEntityLookup();    //TODO make global
-        $entity = $lookup->getEntity($itemId);
-        $aliases = $entity->getAliases("de");
-        $label = array($entity->getLabel("de"));                                    //cast to array for merging in next line
-        $terms = array_merge($aliases, $label);
-
-        return $terms;
+    function getTerms( $wd_entity_id ){
+        /*** get terms (label + aliases) of related wd_item ***/
+        $lookup = WikibaseRepo::getDefaultInstance()->getStore()->getEntityLookup();                //TODO make global
+        $wd_entity = $lookup->getEntity($wd_entity_id);
+        $wd_entity_aliases = $wd_entity->getAliases("de");                                          //TODO get language from db
+        $wd_entity_label = array($wd_entity->getLabel("de")); // cast to array for merging in next line
+        $wd_entity_terms = array_merge($wd_entity_aliases, $wd_entity_label);
+        return $wd_entity_terms;
     }
 
-    function getExternalEntity( $db, $pid, $extId ) {
-        $result = $db->select( "wdq_external_data", "external_data", array( "pid=$pid", "external_id=$extId" ), __METHOD__ );
+    function getExternalEntity( $db, $pid, $external_entity_id ) {
+        /*** get entity of db beloning to $pid and $external_entity_id ***/
+        $result = $db->select( "wdq_external_data", "external_data", array( "pid=$pid", "external_id=$external_entity_id" ), __METHOD__ );
         foreach ( $result as $row ) {
-            return $row->entity_data;
+            return $row->entity_data; // we only gets 1 result!
         }
     }
 
-    function evaluateMapping( $extEntity, $mapping ) {
+    function evaluateMapping( $external_entity, $mapping ) {
         $doc = new DomDocument();
-        $doc->loadXML( $extEntity );
+        $doc->loadXML( $external_entity );
         $domXpath = new DOMXPath( $doc );
         $result = $domXpath->evaluate( $mapping["nodeSelector"] );
 
